ee538fdc7102820d0a23042cf6c8c20d0bcdb5c7
huangbo <huangbo@bogon.(none)>
2013-01-24 08:52:35 +0800

    Initial Commit

---
+++ XMLParserDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XMLParserDemo
+//
+//  Created by huangbo on 13-1-24.
+//  Copyright (c) 2013年 langoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XMLParserDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XMLParserDemo
+//
+//  Created by huangbo on 13-1-24.
+//  Copyright (c) 2013年 langoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
